f4cd5b6b7544a624968b5ac1dbbd28f38f48e4bb
kodek2000 <josekodek2000@gmail.com>
2019-02-09 22:08:22 +0300

    [Facing trouble with testing]

---
+++ manage.py
@@ -0,0 +1,25 @@
+import unittest
+from app import db, create_app
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app=create_app(config_name='development')
+
+migrate=Migrate(app, db)
+manager=Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    tests=unittest.TestLoader().discover('./Tests', pattern='unit*.py')
+    report=unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if report.wasSuccessful():
+        return 1
+    
+    else:
+        return 0
+
+if __name__ == "__main__":
+    manager.run()
